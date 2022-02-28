518f8c789a4f698d966cf0af06f212d16e6683ae
PearEasy <peareasy@life.com>
2022-02-28 10:02:34 +0000

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dtos/login.dto';
+import { RpcException } from '@nestjs/microservices';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  users = [
+    {
+      username: 'admin',
+      password: 'password',
+    },
+  ];
+
+  login(loginDto: LoginDto): string {
+    const idx = this.users.findIndex(
+      (u) =>
+        u.username === loginDto.username && u.password === loginDto.password,
+    );
+
+    if (idx === -1) throw new RpcException('Unauthorized');
+
+    return this.jwtService.sign({ username: loginDto.username });
+  }
+}
